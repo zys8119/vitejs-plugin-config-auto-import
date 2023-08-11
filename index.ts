73596e944e18840ae64209b547212a180805757f
zys8119 <770959294@qq.com>
2023-08-11 17:09:12 +0800

    [add] v0.0.3

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import {App} from "vue"
-const config = [{"data":{"__$__type":"test"},"globalName":"__$__config","__$__type":"__$__config"},{"data":{"b":"asdasd","c":"阿萨德发生打卡","__$__GGGG":"test"},"globalName":"__$__aaa","__$__GGGG":"__$__aaa"}]
+const config = [{"data":{"__$__type":"test"},"globalName":"__$__config","__$__type":"__$__config"}]
 export const install = (app:App)=>{
     JSON.parse(JSON.stringify(config).replace(/__\$__/g,'\$')).forEach(data=>{
         app.config.globalProperties[data.globalName] = data.data
--- types.ts
+++ types.ts
@@ -1,4 +1,4 @@
-import {config0, active0, config1, active1} from "./data";
+import {config0, active0} from "./data";
 export type ViteOptions  = {
     globalName:string
     globalActive:string
@@ -8,16 +8,11 @@ export type ViteOptions  = {
 type GlobalVariableType0 = typeof config0[typeof active0] & {
     $type:typeof active0
 }
-type GlobalVariableType1 = typeof config1[typeof active1] & {
-    $GGGG:typeof active1
-}
 declare module 'vue' {
     export interface ComponentCustomProperties {
         $config:GlobalVariableType0
-        $aaa:GlobalVariableType1
     }
 }
 declare global {
     const $config:GlobalVariableType0
-    const $aaa:GlobalVariableType1
 }
